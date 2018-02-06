685ec9e93f42230f0f6fd7211d30a06ba951db01
mignepo <mig.nepo@gmail.com>
2018-02-06 10:19:13 -0800

    initial commit

---
+++ MyFirstProject/src/Lab0b.java
@@ -0,0 +1,75 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 333;
+		int intOne = 29999;
+		long longOne = 1119999;
+		//decimal variables
+		float floatOne = 328.4444f;
+		double doubleOne = 22666634.22;
+		//other integer types
+		char charOne;
+		charOne = 5;
+
+		//other types
+		boolean booleanOne = false;
+		String stringOne = "McNuggets";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Miguel Nepomuceno       02/1/18*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        real types             *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*        other types            *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = " + booleanOne+"\t\t*");
+		System.out.println("*stringOne = " + stringOne+"\t\t*");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ Unit 2/src/Circle.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Circle
+{
+	private double radius;
+	private double area;
+
+	public static void main(String[] args) {
+		Circle circle1 = new Circle();
+		String rad = JOptionPane.showInputDialog("Enter a double.");
+		double rad2 = Double.parseDouble(rad);
+		circle1.setRadius(rad2);
+		circle1.print();
+	}
+	
+	public void setRadius(double rad)
+	{
+		radius = rad;
+	}
+
+
+	public void calculateArea( )
+	{
+		setRadius(radius);
+		area = 3.14*radius*radius;
+	}
+
+	public void print( )
+	{
+		calculateArea();
+		System.out.println("The area is :: " + area);
+	}
+}
